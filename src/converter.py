9321db44b21f9ec898b260b1f20fdf0b638accd4
Jovasa <jovasa46@gmail.com>
2021-08-13 20:51:57 +0300

    Initial commit

---
+++ src/converter.py
@@ -0,0 +1,24 @@
+
+
+def hiragana_to_katakana(kana):
+    pass
+
+
+def katakana_to_hiragana(kana):
+    pass
+
+
+def hiragana_to_romanji(kana):
+    pass
+
+
+def katakana_to_romanji(kana):
+    pass
+
+
+def romanji_to_katakana(romanji):
+    pass
+
+
+def romanji_to_hiragana(romanji):
+    pass
