72fde719dbcfb339c4e210d5fc1269dddf100a98
Jovasa <jovasa46@gmail.com>
2021-08-14 18:50:28 +0300

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kana_converter',
-    version='0.0.1',
+    version='0.1.1',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
     package_data = {
