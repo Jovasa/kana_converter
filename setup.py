4dee758657cc570ec55ff0fbb84753f6e2d74e61
Jovasa <jovasa46@gmail.com>
2021-08-15 12:09:49 +0300

    Add LICENSE

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='kana_converter',
     version='0.2.0',
+    license="BSD-2-Clause",
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
     package_data = {
