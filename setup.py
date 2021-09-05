7b14563bdb1698eeebf825cedf3d04a3733bdd8f
Jovasa <jovasa46@gmail.com>
2021-09-05 19:26:58 +0300

    Add ヴ kata -> romanji

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kana_converter',
-    version='0.3.1',
+    version='0.3.2',
     license="BSD-2-Clause",
     package_dir={'kana_converter': 'src'},
     packages=["kana_converter"],
